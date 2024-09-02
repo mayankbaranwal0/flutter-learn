8512eb7ce2d39d69faa2752ccb2935247ffc4a33
Mayank Baranwal <mb605508@gmail.com>
2024-09-03 02:17:32 +0530

    Initial commit

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../views/home_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Flutter Learn",
+      home: HomeView(),
+    );
+  }
+}
