2ad26bc6ece062e6c288370644af0171502137f5
Mayank Baranwal <mb605508@gmail.com>
2024-09-03 21:38:03 +0530

    Calculator completed with tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,40 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_learn/app/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets("Hello World Test", (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+  group("Calculator View Test", () {
+    testWidgets("Calculator Basic Test", (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+      expect(find.byKey(const Key("Result")), findsOneWidget);
+      expect(find.byKey(const Key("displayOne")), findsOneWidget);
+      expect(find.byKey(const Key("displayTwo")), findsOneWidget);
+      expect(find.byIcon(CupertinoIcons.add), findsOneWidget);
+      expect(find.byIcon(CupertinoIcons.minus), findsOneWidget);
+      expect(find.byIcon(CupertinoIcons.multiply), findsOneWidget);
+      expect(find.byIcon(CupertinoIcons.divide), findsOneWidget);
+    });
+    
+    testWidgets("Operation Test", (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+      await tester.enterText(find.byKey(const Key("displayOne")), "20");
+      await tester.enterText(find.byKey(const Key("displayTwo")), "10");
 
-    expect(find.byType(Container), findsOneWidget);
-    expect(find.byType(Center), findsOneWidget);
-    expect(find.text("Hello World"), findsOneWidget);
+      await tester.tap(find.byIcon(CupertinoIcons.add));
+      await tester.pump();
+      expect(find.text("30"), findsOneWidget);
+
+      await tester.tap(find.byIcon(CupertinoIcons.minus));
+      await tester.pump();
+      expect(find.text("10"), findsNWidgets(2));
+
+      await tester.tap(find.byIcon(CupertinoIcons.multiply));
+      await tester.pump();
+      expect(find.text("200"), findsOneWidget);
+
+      await tester.tap(find.byIcon(CupertinoIcons.divide));
+      await tester.pump();
+      expect(find.text("2.0"), findsOneWidget);
+    });
   });
 }
